9648902b87134052aa422a0533ea29d50f324372
SantiMontoyao <santiagomontoyao@yahoo.com>
2022-06-19 23:52:23 -0300

    Primer Commit

---
+++ scripts.js
@@ -0,0 +1,81 @@
+// PRIMEROS BOTONES
+function ocultarCard1 (){
+    document.getElementById('matchCards1').style.backgroundColor = '#BA1B1D';
+}
+function match1 (){
+    document.getElementById('matchCards1').style.backgroundColor = '#4AAD52';}
+
+
+// SEGUNDOS BOTONES
+function ocultarCard2 (){
+    document.getElementById('matchCards2').style.backgroundColor = '#BA1B1D';}
+function match2 (){
+    document.getElementById('matchCards2').style.backgroundColor = '#4AAD52';}
+
+// TERCEROS BOTONES
+function ocultarCard3 (){
+    document.getElementById('matchCards3').style.backgroundColor = '#BA1B1D';}
+function match3 (){
+    document.getElementById('matchCards3').style.backgroundColor = '#4AAD52';}
+
+// CUARTOS BOTONES
+function ocultarCard4 (){
+    document.getElementById('matchCards4').style.backgroundColor = '#BA1B1D';}
+function match4 (){
+    document.getElementById('matchCards4').style.backgroundColor = '#4AAD52';}
+
+
+// QUINTOS BOTONES
+function ocultarCard5 (){
+    document.getElementById('matchCards5').style.backgroundColor = '#BA1B1D';}
+function match5 (){
+    document.getElementById('matchCards5').style.backgroundColor = '#4AAD52';}
+
+// SEXTOS BOTONES
+function ocultarCard6 (){
+    document.getElementById('matchCards6').style.backgroundColor = '#BA1B1D';}
+function match6 (){
+    document.getElementById('matchCards6').style.backgroundColor = '#4AAD52';}
+
+// 7mos BOTONES
+function ocultarCard7 (){
+    document.getElementById('matchCards7').style.backgroundColor = '#BA1B1D';}
+function match7 (){
+    document.getElementById('matchCards7').style.backgroundColor = '#4AAD52';}
+
+// 8vos BOTONES
+function ocultarCard8 (){
+    document.getElementById('matchCards8').style.backgroundColor = '#BA1B1D';}
+function match8 (){
+    document.getElementById('matchCards8').style.backgroundColor = '#4AAD52';}
+
+// 9nos BOTONES
+function ocultarCard9 (){
+    document.getElementById('matchCards9').style.backgroundColor = '#BA1B1D';}
+function match9 (){
+    document.getElementById('matchCards9').style.backgroundColor = '#4AAD52';}
+
+
+// 10mos BOTONES
+function ocultarCard10 (){
+    document.getElementById('matchCards10').style.backgroundColor = '#BA1B1D';}
+function match10 (){
+    document.getElementById('matchCards10').style.backgroundColor = '#4AAD52';}
+
+// 11vos BOTONES
+function ocultarCard11 (){
+    document.getElementById('matchCards11').style.backgroundColor = '#BA1B1D';
+    document.getElementById('botonMas').style.display = 'none';
+    document.getElementById('gifCarga').style.display = 'block';}
+function match11 (){
+    document.getElementById('matchCards11').style.backgroundColor = '#4AAD52';
+    document.getElementById('botonMas').style.display = 'none';
+    document.getElementById('gifCarga').style.display = 'block';}
+
+// BOTON CARGAR MÁS
+function cargarMas(){
+    document.getElementById('botonMas').style.display = 'none';
+    document.getElementById('gifCarga').style.display = 'block';
+}
+
+
